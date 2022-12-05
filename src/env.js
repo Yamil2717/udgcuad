490b128e73c64e1270af96af7fca4c00d0cce821
Christian Iglesias <71434195+Yamil2717@users.noreply.github.com>
2022-12-05 10:28:08 -0500

    xd

--- src/env.js
+++ src/env.js
@@ -1,6 +1,6 @@
 const env = {
-  //api: 'https://habitandolametropoli.com/api',
-  api: 'http://192.168.20.20:4000',
+  api: 'https://habitandolametropoli.com/api',
+  //api: 'http://192.168.20.20:4000',
   keyGoogleMaps: 'AIzaSyDpr9PbRVHa-RSJ7i-wie2ISxm4VJoaKF8',
 };
 
